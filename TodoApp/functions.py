3edc62bde1626b1f95dd5e38ed05f1a02ed8f13d
BoyerCoding <BoyerCoding@gmail.com>
2024-09-04 10:14:12 -0400

    Moved functions from main.py to functions.py for better readability.

---
+++ TodoApp/functions.py
@@ -0,0 +1,16 @@
+def get_todos(filepath='todos.txt'):
+    """ This function gets the list of todos from the text file reads the lines and returns a list. """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath='todos.txt'):
+    """ This functions writes the Todos from the list to the todos.txt file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
