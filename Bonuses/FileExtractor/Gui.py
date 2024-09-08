e087ce2232d05faa932f642e5f86a5b29700f349
BoyerCoding <BoyerCoding@gmail.com>
2024-09-08 11:49:57 -0400

    Added functionality to the program

--- Bonuses/FileExtractor/Gui.py
+++ Bonuses/FileExtractor/Gui.py
@@ -1,4 +1,5 @@
 import FreeSimpleGUI as sg
+from func import extract_archive
 
 # layout
 
@@ -6,16 +7,25 @@ sg.theme("DarkPurple4")
 
 zip_label = sg.Text("Choose a File to Extract")
 zip_input = sg.Input()
-zip_button = sg.FileBrowse('Browse')
+zip_button = sg.FileBrowse('Browse', key='File')
 
 destination_label = sg.Text("Choose a Destination Folder")
 destination_input = sg.Input()
-destination_button = sg.FolderBrowse('Browse')
+destination_button = sg.FolderBrowse('Browse', key='Destination')
 
-extract_button = sg.Button('Extract')
+extract_button = sg.Button('Extract', key='Extract')
+output_label = sg.Text(key='Output', text_color='green')
 
-window = sg.Window('File Extractor', layout=[[zip_label,zip_input,zip_button],
-                                             [destination_label,destination_input,destination_button]])
+window = sg.Window('File Extractor',
+                   layout=[[zip_label,zip_input,zip_button],
+                           [destination_label,destination_input,destination_button],
+                           [extract_button, output_label]])
+
 while True:
-    window.read()
-    window.close()
+    event, values = window.read()
+    archivepath = values["File"]
+    dest_dir = values['Destination']
+    extract_archive(archivepath, dest_dir)
+    window["Output"].update(value='Extraction Completed!')
+
+window.close()
