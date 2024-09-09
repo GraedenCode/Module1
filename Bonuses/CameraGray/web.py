4dbea780e7ef42efc3f1dd5bacda5aac13ece421
BoyerCoding <BoyerCoding@gmail.com>
2024-09-09 12:19:42 -0400

    Added an upload feature to Camera Grayscale converter

--- Bonuses/CameraGray/web.py
+++ Bonuses/CameraGray/web.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander('Start Camera'):
     # start camera
     camera_image = st.camera_input('')
@@ -9,6 +11,16 @@ with st.expander('Start Camera'):
 if camera_image:
     # create a pillow image instance
     img = Image.open(camera_image)
+
+    # convert the pillow image to gray scale
+    gray_img = img.convert('L')
+
+    # Render the gray scale image on the webpage
+    st.image(gray_img)
+
+if uploaded_image:
+    # create a pillow image instance
+    img = Image.open(uploaded_image)
 
     # convert the pillow image to gray scale
     gray_img = img.convert('L')
